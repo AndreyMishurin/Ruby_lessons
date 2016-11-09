f7132b04e41e4387973c98ff89afccbf334da0e6
Andrey <asmishurin@gmail.com>
2016-11-09 12:42:16 +0300

    third_attempt

--- lessons_1/lesson_1.4.rb
+++ lessons_1/lesson_1.4.rb
@@ -4,14 +4,16 @@ b = gets.chomp.to_f
 c = gets.chomp.to_f
 
 dis = b ** 2 - 4 * a * c
-x1 = (- b + Math.sqrt(dis)) / 2 * a
-x2 = (- b - Math.sqrt(dis)) / 2 * a
+puts "Дискриминант = #{dis}"
 
-if dis < 0
-	puts "#{dis}, корней нет!"
-elsif dis > 0
-  puts "#{dis}, #{x1}; #{x2}"
+if dis >= 0
+  x1 = (- b + Math.sqrt(dis)) / 2 * a
+  puts "Корень 1 = #{x1}"
+
+  if dis > 0
+    x2 = (- b - Math.sqrt(dis)) / 2 * a
+    puts "Корень 2 = #{x2}"
+  end
 else
-  dis == 0
-  puts "#{dis}, #{x1}"
+  puts "Корней нет!"
 end
