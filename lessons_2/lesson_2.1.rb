958f2e0397c654c0dbe6b88a65428a1c8fb2fcc4
Andrey <asmishurin@gmail.com>
2016-11-11 14:46:31 +0300

    Lessons_2

---
+++ lessons_2/lesson_2.1.rb
@@ -0,0 +1,4 @@
+hash = { 'January' => 31, 'February' => 29, 'March' => 31, 'April' => 30, 'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31, 'September' => 30, 'October' => 31, 'November' => 30, 'December' => 31 }
+
+#a = hash.values
+hash.each { |key, value| puts key if value == 30 }
---
+++ lessons_2/lesson_2.2.rb
@@ -0,0 +1,6 @@
+array = []
+
+a = (10..100).step(5)
+array.push(a.to_a)
+
+puts array
