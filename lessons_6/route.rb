0a0ae2df31b0d4039c93b57923b31b973fb6e7ba
Andrey <asmishurin@gmail.com>
2016-12-08 10:01:02 +0300

    Fixed3

--- lessons_6/route.rb
+++ lessons_6/route.rb
@@ -3,6 +3,7 @@ class Route
     @start_station = start_station
     @finish_station = finish_station
     @stations = [start_station, finish_station]
+    validate!
   end
 
   def set_station(station)
@@ -16,4 +17,18 @@ class Route
   def show_station
     puts stations.split(", ")
   end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+protected
+
+  def del_validate!
+    raise "Вы не можете удалить начальную или конечную станцию" if @start_station == @stations || @finish_station == @stations
+    true
+  end
+
 end
